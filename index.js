dce0af3ade7eb3141a44cf93fb7f2874c0f47666
Yuri Strazhnov <theyuri@mail.ru>
2018-11-26 19:17:23 +0300

    Improved to work. But still testing

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ let holding = {};
 
 const l = text => {
   if (holding.text && holding.showed) {
-    process.stdout.write(ansi.eraseLines(holding.lines));
-    process.stdout.write(ansi.cursorLeft);
+    process.stdout.write(ansi.eraseLines(holding.lines)); // стираем линии занятые старым холдом
+    process.stdout.write(ansi.cursorLeft); // передвигаем курсор до конца налево
   }
   process.stdout.write(text + '\n' + (holding.text || ''));
   if (holding.text && !holding.showed) holding.showed = true;
+
 }
+// text passed into this function will be kept always at the bottom of the console
 l.hold = text => {
+  if (holding.text) {
+    process.stdout.write(ansi.eraseLines(holding.lines));
+    process.stdout.write(ansi.cursorLeft);
+  }
   holding.text = text;
   holding.lines = text.split('\n').length;
   holding.showed = false;
 }
+
+// stop keeping the text and remove it
 l.stop = () => {
 	if (!holding.text) return;
   process.stdout.write(ansi.eraseLines(holding.lines));
